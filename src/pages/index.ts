cb31bf3ee7f68db39b5a1b09761d2e410f8b1bcb
Daxiongzhang <870928829@qq.com>
2026-02-13 23:57:21 +0800

    Deploy v3 to GitHub Pages

--- src/pages/index.ts
+++
@@ -1,7 +0,0 @@
-export { HomePage } from './HomePage';
-export { ResourcesPage } from './ResourcesPage';
-export { ToolsPage } from './ToolsPage';
-export { CommunityPage } from './CommunityPage';
-export { WhitepaperPage } from './WhitepaperPage';
-export { FAQPage } from './FAQPage';
-export { SocialPage } from './SocialPage';
