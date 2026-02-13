8e6ae82b1d3d721660bbc7d24d733e610ff1bdd2
Daxiongzhang <870928829@qq.com>
2026-02-13 14:51:10 +0800

    Fix base path to /v3/

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/wa-protocol-whitepaper/',
+    base: '/v3/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
